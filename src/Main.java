4d40058f6b5e52e0fb662e9fadc55ede89f10d87
mayur9552 <mayur2618006@gmail.com>
2023-02-06 09:47:42 -0500

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,45 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
+public class Main{
 
-        System.out.println("Hello world!");
-        System.out.println("with new commit Hello world!");
+    static int fact(int n){
+        if(n==0)return 1;
+        else{
+            return (n * fact(n-1));
+        }
     }
+
+    public static void main(String[] args){
+
+      Scanner sc = new Scanner(System.in);
+
+      System.out.println("Enter any number, I will give you a Factorial");
+
+      int n = sc.nextInt();
+
+      System.out.println("There are 2 Ways.\n1. with loop\n2. with recursive\nChoose any one");
+
+      int op = sc.nextInt();
+        int r=1;
+
+      switch(op){
+
+          case 1:
+              while(n>0){
+                  r = r*n;
+                  n--;
+              }
+              System.out.println("It was easy, Factorial is "+r);
+              break;
+
+          case 2:
+              System.out.println("It was not hard, Factorial is "+fact(n));
+              break;
+          default:
+              System.out.println("Invalid option");
+      }
+
+
+
+    }
+
 }
