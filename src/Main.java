27788ff57162360a1718d52078bfbd90884eb8a6
mayur9552 <mayur2618006@gmail.com>
2023-02-12 22:14:05 -0500

    Program updated

--- src/Main.java
+++ src/Main.java
@@ -1,102 +1,70 @@
 import java.util.Scanner;
 public class Main{
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
 
-     public static int fact(int n){
-
+    public static int fact(int n){
         if(n==0) return 1;
         else{
             return (n*fact(n-1));
         }
     }
 
-     public static void factorial(){
-         Scanner sc = new Scanner(System.in);
-
-        System.out.println("There are two ways:");
-        System.out.println("1. With Loop");
-        System.out.println("2. With Recursion");
-        System.out.printf("Select any one from above: ");
-        int op = sc.nextInt();
+     public static void factorial(int op,int n){
 
         switch(op){
             case 1:
-                System.out.printf("Enter any number, you will get Factorial: ");
-                int n = sc.nextInt();
                 int a=n;
                 int r = 1;
                 while(n>0){
                     r=r*n;
                     n--;
                 }
-
                 System.out.println("Not Hard, Factorial of " + a + " is " + r);
                 break;
 
             case 2:
-                System.out.printf("Enter any number, you will get Factorial: ");
-                int n1 = sc.nextInt();
-                System.out.println("Not hard, Factorial of " + n1 + " is " + fact(n1));
+                System.out.println("Not hard, Factorial of " + n + " is " + fact(n));
                 break;
         }
-
     }
 
 
-     public static int summ(int n){
-
-        if(n==0) return 0;
+     public static int summ(int a){
+        if(a==0) return 0;
         else{
-            return (n+summ(n-1));
+            return (a+summ(a-1));
         }
     }
-     public static void sum(){
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("There are two ways:");
-        System.out.println("1. With Loop");
-        System.out.println("2. With Recursion");
-        System.out.printf("Select any one from above: ");
-        int op = sc.nextInt();
-
-        switch(op){
+     public static void sum(int opt1, int a){
+        switch(opt1){
             case 1:
-                System.out.printf("Enter any number, you will get sum till 0: ");
-                int n = sc.nextInt();
-                int a=n;
+                int x=a;
                 int r = 0;
-                while(n>0){
-                    r=r+n;
-                    n--;
+                while(a>0){
+                    r=r+a;
+                    a--;
                 }
-
-                System.out.println("Not Hard, Sum of first " + a + " is " + r);
+                System.out.println("Not Hard, Sum of first " + x + " is " + r);
                 break;
 
             case 2:
-                System.out.printf("Enter any number, you will get Sum till 0: ");
-                int n1 = sc.nextInt();
-                System.out.println("Not hard, sum  of first " + n1 + " is " + summ(n1));
+                System.out.println("Not hard, sum  of first " + a + " is " + summ(a));
                 break;
         }
 
     }
 
 
-     public static void rev(){
-
-        Scanner sc = new Scanner(System.in);
-
-        System.out.printf("Enter any number: ");
-        int num = sc.nextInt();
+     public static void rev(int num){
         int n = num;
         int res=0;
-
         while(n!=0){
            res = res*10 + n%10;
            n = n/10;
         }
-
-
         System.out.println("Not Hard, Reverse of " + num + " is " + res);
     }
 
@@ -120,24 +88,19 @@ public class Main{
                odd = odd + num;
            }
        }
-
         System.out.println("sum of odd = " + odd + " and sum of even = " + even);
-
     }
 
 
      public static void untiln(){
+         Scanner sc = new Scanner(System.in);
         int i=0;
         int sum = 0;
-        while(i>=0){
-
-            Scanner sc = new Scanner(System.in);
-
+        do{
+            sum=sum+i;
             System.out.printf("Enter number: ");
-            int n =  sc.nextInt();
-            if(n<0) break;
-            sum = sum + n;
-        }
+            i =  sc.nextInt();
+        }while(i>=0);
         System.out.println("sum  of all is " + sum);
     }
 
@@ -189,9 +152,12 @@ public class Main{
     }
 
 
-     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+     public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
 
+         int op=1;
+         do {
+             clearScreen();
              System.out.println("Welcome to Java Assignment\n");
              System.out.println("1. factorial");
              System.out.println("2. Sum of first N number");
@@ -202,20 +168,38 @@ public class Main{
              System.out.println("7. Diagnol Star Pattern");
              System.out.println("8. Decreasing Star Pattern");
              System.out.println("9. Increasing Star Pattern");
+             System.out.println("0. Exit");
              System.out.printf("Choose from above: ");
-             int op = sc.nextInt();
+             op = sc.nextInt();
 
              switch (op) {
                  case 1:
-                     factorial();
+                     System.out.println("There are two ways:");
+                     System.out.println("1. With Loop");
+                     System.out.println("2. With Recursion");
+                     System.out.printf("Select any one from above: ");
+                     int opt = sc.nextInt();
+                     System.out.printf("Enter any number, you will get Factorial: ");
+                     int n = sc.nextInt();
+                     factorial(opt, n);
                      break;
 
                  case 2:
-                     sum();
+
+                     System.out.println("There are two ways:");
+                     System.out.println("1. With Loop");
+                     System.out.println("2. With Recursion");
+                     System.out.printf("Select any one from above: ");
+                     int opt1 = sc.nextInt();
+                     System.out.printf("Enter any number, you will get sum till 0: ");
+                     int a = sc.nextInt();
+                     sum(opt1, a);
                      break;
 
                  case 3:
-                     rev();
+                     System.out.printf("Enter any number: ");
+                     int num = sc.nextInt();
+                     rev(num);
                      break;
 
                  case 4:
@@ -242,10 +226,9 @@ public class Main{
                      incStar();
                      break;
 
-
                  default:
                      System.out.printf("Invalid option. Try again!");
              }
-
-    }
+         }while(op!=0);
+     }
 }
